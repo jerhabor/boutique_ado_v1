6e5bcebc8f2b81f267840a4b692967a665620457
Jesse Erhabor <jess.erhabor@gmail.com>
2021-03-22 11:47:17 +0000

    Customized admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,30 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image'
+    )
+
+    ordering = ('sku',)  # this sorts products by SKU
+    # needs to be a tuple since it's possible to sort on multiple columns
+    # to reverse it, we can add a '-' infront of 'sku'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+# Register new classes above alongside their respective models
+admin.site.register(Category, CategoryAdmin)
