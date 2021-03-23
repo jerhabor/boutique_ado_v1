e0191f999b3f7032c07275c45dbd9ce176c21f54
Jesse Erhabor <jess.erhabor@gmail.com>
2021-03-23 15:53:02 +0000

    updated context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,6 +3,8 @@ from decimal import Decimal
 instead of float because it is more accurate.
 Float is susceptible to rounding errors'''
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -10,6 +12,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
