1d44d1b62eb0581b8a7c3539ce92dfb4523dbd79
Jesse Erhabor <jess.erhabor@gmail.com>
2021-04-02 11:10:29 +0000

    App now deployed to Heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eaw%#d7e+w9s-d#ds&)@8t-q%3)dm^06t7wnalohxo5d#2q=w)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['je-boutique-ado.herokuapp.com', 'localhost']
 
